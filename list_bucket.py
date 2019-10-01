d4b144f2d15e13e1e7b341de3229a096482a28eb
sureshvenkey <50266914+sureshvenkey@users.noreply.github.com>
2019-10-01 09:43:26 +0530

    Delete list_bucket.py

--- list_bucket.py
+++
@@ -1,5 +0,0 @@
-import boto3
-s3_res=boto3.resource("s3",aws_access_key_id="AKIAJZKRSWXKFQ5KDI4Q", aws_secret_access_key="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", region_name="ap-south-1")
-for each_bucket in s3_res.buckets.all():
-	print (each_bucket.name)
-
